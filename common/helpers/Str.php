f38608ab296bea755e76085a3f9a06b692977689
likai <youyuge@gmail.com>
2013-08-16 23:19:52 +0800

    Add string random helper

---
+++ common/helpers/Str.php
@@ -0,0 +1,16 @@
+<?php
+
+class Str
+{
+    public static function random($length)
+    {
+        static $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        $string = '';
+        for ($i = 0; $i < $length; $i++) {
+            $string .= $chars[rand(0, strlen($chars) - 1)];
+        }
+
+        return $string;
+    }
+}
